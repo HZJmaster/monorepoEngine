131620278c88aff2d5981840e46edceeccfa402f
HZJMaster <846163332@qq.com>
2024-06-02 16:04:38 +0800

    feat(all): use husky to create git hook

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # monorepoEngine
+
 monorepoEngine
--- apps/ex-app/src/index.ts
+++ apps/ex-app/src/index.ts
@@ -1,7 +1,7 @@
-import { add } from '@monorepo-engine/utils'
+import { add } from '@monorepo-engine/utils';
 
 function main() {
-    console.log(add(1, 2));
+  console.log(add(1, 2));
 }
 
-main()
+main();
